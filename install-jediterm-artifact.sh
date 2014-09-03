35b11aba8b9e0017b6a818f1e1b35e6bff171022
Adam Wyłuda <adamwyl92@gmail.com>
2014-09-03 14:40:11 +0200

    Add install-jediterm-artifact.sh

---
+++ install-jediterm-artifact.sh
@@ -0,0 +1,13 @@
+# This script installs JediTerm library as Maven artifact into local repository
+
+# Usage ./install-jediterm-artifact "path_to_jediterm_build_directory/jediterm-pty-2.0.jar"
+
+JAR_PATH=$1
+
+if [ -z "$JAR_PATH" ]
+then
+    echo "Please provide path to jediterm-pty-2.0.jar as script argument"
+    exit 1
+fi
+
+mvn install:install-file -Dfile=$JAR_PATH -DgroupId=com.jediterm -DartifactId=jediterm-pty -Dversion=2.0 -Dpackaging=jar
