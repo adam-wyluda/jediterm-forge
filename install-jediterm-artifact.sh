8071cd0a8a1b851ea22a35e147fc1f169845a036
Adam Wyłuda <adamwyl92@gmail.com>
2014-09-03 16:53:57 +0200

    Change dependency to jediterm-terminal

--- install-jediterm-artifact.sh
+++ install-jediterm-artifact.sh
@@ -1,13 +1,13 @@
 # This script installs JediTerm library as Maven artifact into local repository
 
-# Usage ./install-jediterm-artifact "path_to_jediterm_build_directory/jediterm-2.0.jar"
+# Usage ./install-jediterm-artifact "path_to_jediterm_build_directory/jediterm-terminal-2.0.jar"
 
 JAR_PATH=$1
 
 if [ -z "$JAR_PATH" ]
 then
-    echo "Please provide path to jediterm-2.0.jar as script argument"
+    echo "Please provide path to jediterm-terminal-2.0.jar as script argument"
     exit 1
 fi
 
-mvn install:install-file -Dfile=$JAR_PATH -DgroupId=com.jediterm -DartifactId=jediterm-pty -Dversion=2.0 -Dpackaging=jar
+mvn install:install-file -Dfile=$JAR_PATH -DgroupId=com.jediterm -DartifactId=jediterm-terminal -Dversion=2.0 -Dpackaging=jar
