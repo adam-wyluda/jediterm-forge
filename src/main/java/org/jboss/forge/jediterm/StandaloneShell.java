64987dd0ae34094e5433c44213f6cd0d8af42aeb
Adam Wyłuda <adamwyl92@gmail.com>
2014-09-03 17:53:44 +0200

    Add StandaloneShell

---
+++ src/main/java/org/jboss/forge/jediterm/StandaloneShell.java
@@ -0,0 +1,27 @@
+package org.jboss.forge.jediterm;
+
+import com.jediterm.terminal.ui.JediTermWidget;
+import com.jediterm.terminal.ui.settings.DefaultSettingsProvider;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Adam Wyłuda
+ */
+public class StandaloneShell extends JFrame
+{
+    public static void main(String[] args)
+    {
+        JediTermWidget widget = new JediTermWidget(new DefaultSettingsProvider());
+        widget.getComponent();
+
+        new StandaloneShell(widget).setVisible(true);
+    }
+
+    public StandaloneShell(JediTermWidget widget) throws HeadlessException
+    {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setContentPane(widget.getComponent());
+    }
+}
