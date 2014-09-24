447962da3eb80600576865bbc3b92cb812107d81
Adam Wyłuda <adamwyl92@gmail.com>
2014-09-24 13:36:20 +0200

    Start Furnace in standalone terminal

--- standalone-terminal/src/main/java/org/jboss/forge/jediterm/StandaloneTerminal.java
+++ standalone-terminal/src/main/java/org/jboss/forge/jediterm/StandaloneTerminal.java
@@ -2,19 +2,26 @@ package org.jboss.forge.jediterm;
 
 import com.jediterm.terminal.ui.JediTermWidget;
 import org.jboss.forge.furnace.Furnace;
+import org.jboss.forge.furnace.repositories.AddonRepositoryMode;
 import org.jboss.forge.furnace.se.FurnaceFactory;
+import org.jboss.forge.furnace.util.OperatingSystemUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 
 /**
  * @author Adam Wyłuda
  */
 public class StandaloneTerminal extends JFrame
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         Furnace furnace = FurnaceFactory.getInstance();
+
+        String addonDir = new File(OperatingSystemUtils.getUserForgeDir(), "addons").getAbsolutePath();
+        furnace.addRepository(AddonRepositoryMode.MUTABLE, new File(addonDir));
+        furnace.startAsync().get();
 
         JediTermWidget widget = ForgeConsoleFactory.create(furnace);
         widget.getComponent();
